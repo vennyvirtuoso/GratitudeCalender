e20899564e3e5bed1b3e623a23823d7e509f824e
vennyvirtuoso <vijendra9753@gmail.com>
2023-03-25 17:34:36 +0530

    Initial Commit

---
+++ GratitudeCalender/GratitudeCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratitudeCalenderApp.swift
+//  GratitudeCalender
+//
+//  Created by Vijendra Vaishya on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GratitudeCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
